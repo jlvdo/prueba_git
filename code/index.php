6ce089b5faeec99962441ff881420e070c7c9ad6
jesus <jesus.vazquezdelolmo@gmail.com>
2022-02-22 18:00:21 +0100

    Agregando tre.php

--- code/index.php
+++ code/index.php
@@ -2,4 +2,6 @@
     echo "<h1> Bienvenidos a App I</h1>";
     echo "<br>";
     echo "<a href='dos.php'>Haz click para ir a dos.php</a>";
+    echo "<br>";
+    echo "<a href='tres.php'>Haz click para ir a tres.php</a>";
 ?>
